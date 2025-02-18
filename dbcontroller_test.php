da4714e60ed83c7bfa5bdd3b855f36263dadc2e2
dominikgolarics <72364663146@szily.hu>
2025-02-18 10:37:14 +0100

    dbcontroller teszt elindult

---
+++ dbcontroller_test.php
@@ -0,0 +1,14 @@
+<?php
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
+require_once("dbcontroller.php");
+
+$db = new DBController();
+if($db->connectDB()){
+    echo "Az adatbázis kapcsolat sikeres <br>";
+}
+else{
+    echo "Az adatbázis kapcsolat sikertelen <br>";
+}
